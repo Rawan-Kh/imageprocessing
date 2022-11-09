1b55de5200ab3447af388a2c09e27be7f1e77a40
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2022-11-09 11:13:21 +0200

    Update contour.py

--- contour.py
+++ contour.py
@@ -9,3 +9,20 @@ contours = measure.find_contours(horse_image,0.8)
 
 # Shows the image with contours found
 show_image_contour(horse_image, contours)
+
+
+# colored image contouring
+# Make the image grayscale
+image_dice = color.rgb2gray(image_dice)
+
+# Obtain the optimal thresh value
+thresh = filters.threshold_otsu(image_dice)
+
+# Apply thresholding
+binary = image_dice > thresh
+
+# Find contours at a constant value of 0.8
+contours = measure.find_contours(binary, 0.8)
+
+# Show the image
+show_image_contour(image_dice, contours)
